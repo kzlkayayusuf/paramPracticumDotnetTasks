bbf1ae9d2216a30ca9b123c61cd1713517c00289
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2023-02-24 16:36:00 +0300

    Coded MappingProfile

---
+++ dotnet_tasks/MovieStore/WebAPI/Utilities/AutoMapper/MappingProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using Entities.Dtos;
+using Entities.Models;
+
+namespace WebAPI.Utilities.AutoMapper;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<MovieDtoForUpdate, Movie>();
+    }
+}
