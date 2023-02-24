468fa046d129d3f174db6cfdea7297b3cae25c12
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2023-02-24 20:52:08 +0300

    Coded Custom Csv OutputFormatter

---
+++ dotnet_tasks/MovieStore/WebAPI/Extensions/IMvcBuilderExtensions.cs
@@ -0,0 +1,11 @@
+using WebAPI.Utilities.Formatters;
+
+namespace WebAPI.Extensions;
+
+public static class IMvcBuilderExtensions
+{
+    public static IMvcBuilder AddCustomCsvFormatter(this IMvcBuilder builder) =>
+        builder.AddMvcOptions(config =>
+                config.OutputFormatters.Add(new CsvOutputFormatter())
+            );
+}
