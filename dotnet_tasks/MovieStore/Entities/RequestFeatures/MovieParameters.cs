863e532991749f686eea93e3569de50bf36d6e43
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2023-02-25 22:23:08 +0300

    Started Pagination

---
+++ dotnet_tasks/MovieStore/Entities/RequestFeatures/MovieParameters.cs
@@ -0,0 +1,6 @@
+namespace Entities.RequestFeatures;
+
+public class MovieParameters : RequestParameters
+{
+
+}
---
+++ dotnet_tasks/MovieStore/Entities/RequestFeatures/RequestParameters.cs
@@ -0,0 +1,17 @@
+namespace Entities.RequestFeatures;
+
+public abstract class RequestParameters
+{
+    const int maxPageSize = 50;
+
+    // auto-implemented property
+    public int PageNumber { get; set; }
+
+    // full-property
+    private int pageSize;
+    public int PageSize
+    {
+        get { return pageSize; }
+        set { pageSize = value > maxPageSize ? maxPageSize : value; }
+    }
+}
