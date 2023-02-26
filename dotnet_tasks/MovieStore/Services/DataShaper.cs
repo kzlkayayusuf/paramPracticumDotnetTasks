c0f5290ec89e4629b92c975b82cb3d9fdeb7dc27
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2023-02-26 17:14:54 +0300

    Coded Data Shaper

---
+++ dotnet_tasks/MovieStore/Services/DataShaper.cs
@@ -0,0 +1,78 @@
+using System.Dynamic;
+using System.Reflection;
+using Services.Contracts;
+
+namespace Services;
+
+public class DataShaper<T> : IDataShaper<T> where T : class
+{
+    public PropertyInfo[] Properties { get; set; }
+
+    public DataShaper()
+    {
+        Properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+    }
+
+    public IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldsString)
+    {
+        // ExpandoObject -> run time da dinamik olarak ürettiğimiz herhangi bir nesneye karşılık gelir.
+        var requiredProperties = GetRequiredProperties(fieldsString);
+        return FetchData(entities, requiredProperties);
+    }
+
+    public ExpandoObject ShapeData(T entity, string fieldsString)
+    {
+        var requiredProperties = GetRequiredProperties(fieldsString);
+        return FetchDataForEntity(entity, requiredProperties);
+    }
+
+    private IEnumerable<PropertyInfo> GetRequiredProperties(string fieldsString)
+    {
+        var requiredFields = new List<PropertyInfo>();
+        if (!string.IsNullOrWhiteSpace(fieldsString))
+        {
+            var fields = fieldsString.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var field in fields)
+            {
+                var property = Properties.FirstOrDefault(pi => pi.Name.Equals(field.Trim(), StringComparison.InvariantCultureIgnoreCase));
+                if (property is null)
+                    continue;
+
+                requiredFields.Add(property);
+            }
+
+        }
+        else
+        {
+            requiredFields = Properties.ToList();
+        }
+
+        return requiredFields;
+    }
+
+    private ExpandoObject FetchDataForEntity(T entity, IEnumerable<PropertyInfo> requiredProperties)
+    {
+        var shapedObject = new ExpandoObject(); // run time da oluşur
+
+        foreach (var property in requiredProperties)
+        {
+            var objectPropertyValue = property.GetValue(entity);
+            shapedObject.TryAdd(property.Name, objectPropertyValue);
+        }
+        return shapedObject;
+    }
+
+    private IEnumerable<ExpandoObject> FetchData(IEnumerable<T> entities, IEnumerable<PropertyInfo> requiredProperties)
+    {
+        var shapedData = new List<ExpandoObject>();
+
+        foreach (var entity in entities)
+        {
+            var shapedObject = FetchDataForEntity(entity, requiredProperties);
+            shapedData.Add(shapedObject);
+        }
+
+        return shapedData;
+    }
+}
