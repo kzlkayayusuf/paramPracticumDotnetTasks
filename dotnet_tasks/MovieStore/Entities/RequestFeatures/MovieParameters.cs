72ea40745694c30f8b87f5f958d4ba27460cdcd2
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2023-02-26 13:49:06 +0300

    Started Filtering

--- dotnet_tasks/MovieStore/Entities/RequestFeatures/MovieParameters.cs
+++ dotnet_tasks/MovieStore/Entities/RequestFeatures/MovieParameters.cs
@@ -2,5 +2,7 @@ namespace Entities.RequestFeatures;
 
 public class MovieParameters : RequestParameters
 {
-
+    public uint MinPrice { get; set; }
+    public uint MaxPrice { get; set; } = 1000;
+    public bool ValidPriceRange => MaxPrice > MinPrice;
 }
