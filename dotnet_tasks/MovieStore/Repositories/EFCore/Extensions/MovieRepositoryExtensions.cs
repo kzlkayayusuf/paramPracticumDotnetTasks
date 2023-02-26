8acbd1cf457fbf60f45adb029cdb1d473eaea56a
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2023-02-26 16:04:41 +0300

    Coded Order Query Builder Extension Method

--- dotnet_tasks/MovieStore/Repositories/EFCore/Extensions/MovieRepositoryExtensions.cs
+++ dotnet_tasks/MovieStore/Repositories/EFCore/Extensions/MovieRepositoryExtensions.cs
@@ -1,5 +1,3 @@
-using System.Reflection;
-using System.Text;
 using Entities.Models;
 using System.Linq.Dynamic.Core;
 
@@ -25,31 +23,7 @@ public static class MovieRepositoryExtensions
         if (string.IsNullOrWhiteSpace(orderByQueryString))
             return movies.OrderBy(m => m.Id);
 
-        var orderParams = orderByQueryString.Trim().Split(',');
-
-        // reflection
-        var propertyInfos = typeof(Movie).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-        var orderQueryBuilder = new StringBuilder();
-
-        foreach (var param in orderParams)
-        {
-            if (string.IsNullOrWhiteSpace(param))
-                continue;
-
-            var propertyFromQueryName = param.Split(' ')[0];
-            var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName,
-                StringComparison.InvariantCultureIgnoreCase));
-
-            if (objectProperty is null)
-                continue;
-
-            var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-
-            orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
-        }
-
-        var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+        var orderQuery = OrderQueryBuilder.CreateOrderQuery<Movie>(orderByQueryString);
 
         if (orderQuery is null)
             return movies.OrderBy(m => m.Id);
