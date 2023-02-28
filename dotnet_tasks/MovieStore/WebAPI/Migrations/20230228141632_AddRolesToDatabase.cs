0c1362adccd92099af241cf42517d7998fcf8938
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2023-02-28 17:20:11 +0300

    Migration for User Roles

---
+++ dotnet_tasks/MovieStore/WebAPI/Migrations/20230228141632_AddRolesToDatabase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRolesToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "383e055f-8c41-46db-9e47-2bb0ac4d6296", null, "Admin", "ADMIN" },
+                    { "9f0093ee-6da2-46d3-84ac-3c63233f839e", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "383e055f-8c41-46db-9e47-2bb0ac4d6296");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9f0093ee-6da2-46d3-84ac-3c63233f839e");
+        }
+    }
+}
