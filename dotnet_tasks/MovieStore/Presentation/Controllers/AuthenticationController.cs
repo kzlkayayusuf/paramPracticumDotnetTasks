63c2b6eb6f6da46749df2f1a3386d692bc9ad611
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2023-02-28 19:43:53 +0300

    Coded Authentication Controller

---
+++ dotnet_tasks/MovieStore/Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+using Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.ActionFilters;
+using Services.Contracts;
+
+namespace Presentation.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IServiceManager manager;
+
+    public AuthenticationController(IServiceManager manager)
+    {
+        this.manager = manager;
+    }
+
+    [HttpPost]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> Register([FromBody] UserForRegistrationDto userForRegistrationDto)
+    {
+        var result = await manager.AuthenticationService.RegisterUser(userForRegistrationDto);
+
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.TryAddModelError(error.Code, error.Description);
+            }
+            return BadRequest(ModelState);
+        }
+
+        return StatusCode(201);
+    }
+}
